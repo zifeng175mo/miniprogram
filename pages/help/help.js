8087c4c630ac22baac2c592217a0043feec5a49c
frankstorming <1728298218@qq.com>
2020-06-03 22:17:31 +0800

    Basically implement the interface of feedback and modify about message and UserInfo.

--- pages/help/help.js
+++ pages/help/help.js
@@ -5,80 +5,22 @@ Page({
      * 页面的初始数据
      */
     data: {
+        radio1: '1',
+        icon: {
+            normal:
+                'https://img.yzcdn.cn/public_files/2017/10/13/c547715be149dd3faa817e4a948b40c4.png',
+            active:
+                'https://img.yzcdn.cn/public_files/2017/10/13/793c77793db8641c4c325b7f25bf130d.png'
+        }
 
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-        // wx.request({
-        //   url: '',
-        //   data: {
-        //     openid: '',
-        //     type: 'cardlist'
-        //   },
-        //   method: 'POST',
-        //   header: {
-        //     'content-type': 'application/json' //默认值
-        //   },
-        //   success: res=> {
-        //     this.setData({
-        //
-        //     })
-        //
-        //   },
-        //   fail: res=> {
-        //   }
-        // })
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    onChange(event) {
+        console.log('onchange');
+        const { key } = event.currentTarget.dataset;
+        this.setData({ [key]: event.detail });
     }
-})
+});
